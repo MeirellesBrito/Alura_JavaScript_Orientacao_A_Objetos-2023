0f24c979c2bbd398c96222f1827d7f3386353ceb
MeirellesBrito <aluno.f.meirelles@gmail.com>
2023-02-21 02:14:13 -0300

    Alura JS | Inicio do curso da Alura JavaScript: programando a Orientação a Objetos | Null e undefined - 2023

--- ProjetoBanco/index.js
+++ ProjetoBanco/index.js
@@ -5,9 +5,9 @@ const cliente1 = new Cliente();
 cliente1.nome = "Fernando";
 cliente1.cpf = 3355544545454;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 33555455447745; 
+// const cliente2 = new Cliente();
+// cliente2.nome = "Alice";
+// cliente2.cpf = 33555455447745; 
 
 
 const contaCorrente = new ContaCorrente();
@@ -16,11 +16,11 @@ contaCorrente.cliente = cliente1
 contaCorrente.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = cliente2;
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
+conta2.cliente.cpf = 33555455447745;
 conta2.agincia = 102;
 
-let valor = 200
+let valor = 200;
 contaCorrente.tranferir(valor, conta2);
-console.log("valor: ", valor);
 console.log(conta2);
-
